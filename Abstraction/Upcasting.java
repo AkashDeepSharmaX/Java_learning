8b1e00657103d1e3862e0236fcfb44701a286443
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2025-03-07 04:28:46 +0530

    upcasting

--- Abstraction/Upcasting.java
+++ Abstraction/Upcasting.java
@@ -1,18 +1,33 @@
-class Remote{
+abstract class Remote{
+abstract void powerBUtton();
+}
+class Sony extends Remote{
+    @Override
+    public  void powerBUtton(){
+        System.out.println("remote have PowerOff and PowerOn button");
 
-public void powerBUtton(){
-System.out.println("remote have PowerOff and PowerOn button");
-}
-}
-class sony extends Remote{
+    }
     public void navigationButton(){
         System.out.println("sony Remate have Navigation buttons");
 }}
+class Samsung extends Remote{
+    @Override
+    public void powerBUtton(){
+        System.out.println("remote have PowerOff and PowerOn button");
+    }
+    public void voiceComandFeature(){
+        System.out.println("Samsung Remote have voice command feature");
+    }
+}
+
 public class Upcasting{
     public static void main (String[] arg ){
-        Remote remote= new sony();
-        remote.navigationButton();
+        Remote remote; 
+        remote= new Sony();
         remote.powerBUtton();
+        remote = new Samsung();
+        remote.powerBUtton();
+
 
     }
 }

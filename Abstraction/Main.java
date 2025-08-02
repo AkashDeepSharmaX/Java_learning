c15644150df5a3eee8ca173c03dea2d6db65f90e
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2025-08-02 17:41:15 +0530

    astract object and abject class

---
+++ Abstraction/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Car tesla= new tesla();
+        Car porsche = new porsche();
+tesla.startEngine();
+porsche.startEngine();        
+    }
+}
--- Interface/interface_mini_project/Appliance.java
+++ Interface/interface_mini_project/Appliance.java
@@ -1,6 +1,10 @@
 package interface_mini_project;
 
-public interface Appliance{
+public interface Appliance {
     void on();
+    // abstract method implementing classes must define this method
+    // No need to write access modifiers like public or abstract,
+
     void off();
+    // abstract method implementing classes must define this method
 }

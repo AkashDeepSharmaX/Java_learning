3d6faf5e13c657cba0c6afbfd86f8b97f51ccb78
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2025-10-30 08:21:11 +0530

    intial commit

--- Methods/Main.java
+++ Methods/Main.java
@@ -1,11 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        ClassMeth classMeth= new ClassMeth();
-        int m  =classMeth.sum(15, 54);
-        System.out.println("Sum of two no is "+m);
-        public ClassMeth(){
-            
-        }
+//public class Main {
+//    public static void main(String[] args) {
+//        ClassMeth classMeth= new ClassMeth();
+//        int m  =classMeth.sum(15, 54);
+//        System.out.println("Sum of two no is "+m);
+//        new ClassMeth();{
+//
+//        }
 
-    }
-}
+//    }
+//}
--- dsa/Halfpyramid.java
+++ dsa/Halfpyramid.java
@@ -1,10 +1,10 @@
-public class rightaglStar {
+public class Halfpyramid {
     public static void main(String []args){
  // Outer loop controls rows vertical iteration         
         for (int i=0;i<6; i++){           
    // Inner loop controls columns horizontal iteration
          for(int j=0; j<=i ; j++){
-            System.out.print(i+" "+j+"*");
+            System.out.print("*");
        }
  // Move to the next line after each row
         System.out.println();

6d1be8bb29805d6eaa0fe5451b482d6824581241
AkashDeepSharmaX <akashdeepsharma665@gmail.com>
2025-11-19 05:49:23 +0530

    alphabet reverse

---
+++ LoopFoundation/Alphabet.java
@@ -0,0 +1,10 @@
+public  class Alphabet {
+ public static void main (String []args){
+    System.out.println("printing Alphbets");
+    for(char s='a'; s <='z'; s++){
+        System.out.println(s);
+  }
+System.out.println("Thank you!");
+ }
+
+}
---
+++ LoopFoundation/ReverseAlphabet.java
@@ -0,0 +1,10 @@
+public class ReverseAlphabet {
+    public static void main (String []args ){
+        System.out.println("printing reverse alphabet series... ");
+        for(char r='z'; r >='a'; r--){
+            System.out.println("printing..."+r);
+        }
+    }
+
+    
+}
